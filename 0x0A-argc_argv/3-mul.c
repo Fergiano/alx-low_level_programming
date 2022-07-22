3cadfabeaae2f120c77482910e90830856fd7e9f
Temitope <temitope.koya@hotmail.com>
2022-07-22 10:06:18 -0700

    Mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,13 @@
 int main(int argc, char *argv[])
 {
 	int a, b, result;
+
 	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	result = a * b;
