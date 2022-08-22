0aa9ec6d769b837f6fa737fef0953f5a69ab14ed
Temitope <temitope.koya@hotmail.com>
2022-08-22 06:26:58 -0700

    Elf header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - Copies the content of a file to another file.
+ *
+ * @ac: Argument count
+ *
+ * @av: argument values
+ *
+ * Return: 0 on succes, -1 on error.
+ *
+ */
+
+int main(int ac, char *av[])
+{
+	(void)ac;
+	(void)av;
+	return (1);
+	return (0);
+}
