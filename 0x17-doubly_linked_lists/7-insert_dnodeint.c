84feafe15c72b15faf01a5d55b3dacb8acfd996a
Temitope <temitope.koya@hotmail.com>
2022-09-29 10:15:38 -0700

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,52 +2,42 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at given index in the list
- * @head: pointer to head of the list
+ * @h: pointer to head of the list
  * @idx: index to add at, starting from 0
  * @n: value of new node
  * Return: new node or null
  **/
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count;
-	dlistint_t *tmp, *new, *tmp_prev;
+	dlistint_t *prev, *new;
 
-	if (head == NULL && idx > 0)
-	return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!idx)
+		return (add_dnodeint(h, n));
+
+	if (!h)
 		return (NULL);
-	new->n = n, new->prev = new->next = NULL;
 
-	if (idx == 0)
-	{
-		if (*head)
-	{
-		new->next = *head;
-		(*head)->prev = new, *head = new;
-	}
-		else
-			*head = new;
-		return (new);
-	}
-	count = 1, tmp = (*head)->next;
-	while (tmp)
-	{
-		if (idx == count)
-		{
-			tmp->prev->next = new, new->prev = tmp->prev;
-			new->next = tmp, tmp->prev = new;
-			return (new);
-		}
-		count++;
-		tmp_prev = tmp;
-		tmp = tmp->next;
-	}
-	if (tmp == NULL && count == idx)
-	{
-		tmp_prev->next = new, new->prev = tmp_prev;
-		return (new);
-	}
-	free(new);
-	return (NULL);
+	prev = *h;
+
+	if (!prev)
+		return (NULL);
+
+	while (--idx && prev->next)
+		prev = prev->next;
+
+	if (idx)
+		return (NULL);
+
+	new = malloc(sizeof(*new));
+	if(!new)
+		return (NULL);
+
+	new-> = n;
+	new->prev = prev;
+	new->next = prev->next;
+
+	if (prev->next)
+		prev->next->prev = new;
+
+	return (new);
 }
